5c23f4e57bbf1524236f6e4ca390c7f595b50766
ReactiveNeon <39702521+ReactiveNeon@users.noreply.github.com>
2018-10-04 21:01:23 -0400

    Add files via upload

---
+++ Weapon Scripting/Light Weapons/Revolver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Revolver : LightWeapon {
+
+    private void Start()
+    {
+        currentAmmo = clipSize;
+    }
+
+    public override void DoShoot()
+    {
+        base.DoShoot();
+        Debug.Log("BANG");
+    }
+}
