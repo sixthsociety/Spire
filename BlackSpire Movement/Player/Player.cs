db4c9094e4e29e54f641e87cb52a0e327986404e
ReactiveNeon <39702521+ReactiveNeon@users.noreply.github.com>
2018-10-15 22:29:11 -0400

    Add files via upload

---
+++ BlackSpire Movement/Player/Player.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMove))]
+public class Player : MonoBehaviour {
+
+    PlayerMove playerMove;
+    PlayerWeapon playerWeapon;
+
+    private bool inBase;
+
+    void Start () 
+    {
+        playerMove = GetComponent<PlayerMove>();
+        playerWeapon = GetComponentInChildren<PlayerWeapon>();
+    }
+}
