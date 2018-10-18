d2747d07c5c44e08de36c3cc32782c3581a67c0a
Yamoshi <babbas107@gmail.com>
2018-10-18 23:06:03 +0200

    PickUP/Inventory
    So I've started working on the pickup, which is the simplest thing to do, but then I've realised I should work on the inventory to, so I started that too, however it's not finish, I'll finish after this pull.

---
+++ BlackSpire/Assets/Scripts/Weapon Scripting/RocketLauncherPickup.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketLauncherPickup : MonoBehaviour, IInventoryItem
+{
+
+    public string Name
+    {
+        //gets name
+        get
+        {
+            ///enter weapon name here
+            return "RocketLauncher";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ BlackSpire/Assets/Scripts/Weapon Scripting/WeaponPickupTemplate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickupTemplate : MonoBehaviour, IInventoryItem{
+
+    public string Name
+    {
+        //gets name
+        get
+        {
+            ///enter weapon name here
+            return "RocketLauncher";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+}
