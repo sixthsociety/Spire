8c51765a2781c748cd2faa6c4c9371f78b2aa1f1
Oliver Maglana <olivermaglana@protonmail.com>
2018-10-19 01:17:07 +0800

    Added In-Game Menu

---
+++ BlackSpire/Assets/Scripts/UI/InGameMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InGameMenu : MonoBehaviour {
+
+    public Transform menu;
+
+    private void Update() {
+        if (!menu.gameObject.activeSelf && Input.GetKeyDown(KeyCode.Escape)) {
+            menu.gameObject.SetActive(true);
+            Debug.Log(menu.gameObject.activeSelf);
+        }
+        else if(menu.gameObject.activeSelf && Input.GetKeyDown(KeyCode.Escape)) {
+            menu.gameObject.SetActive(false);
+        }
+    }
+
+    public void BackButton() {
+        menu.gameObject.SetActive(false);
+    }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
