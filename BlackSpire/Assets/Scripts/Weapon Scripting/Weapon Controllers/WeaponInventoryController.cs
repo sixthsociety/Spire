4ef4029780461fdbc9249a71598a9af13627bdae
Yamoshi <babbas107@gmail.com>
2018-10-19 19:08:14 +0200

    Inventory/WeaponController
    The weaponcontroller is a template, so for each weapon there's gonna be a different script, since it's looking for the name of the object.

---
+++ BlackSpire/Assets/Scripts/Weapon Scripting/Weapon Controllers/WeaponInventoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponInventoryController : MonoBehaviour , IInventoryItem {
+
+    public string Name
+    {
+        get
+        {
+            return "RocketLauncher";
+        }
+    }
+
+    public Sprite _Image;
+
+    public Sprite Image
+    {
+        get { return _Image; }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+}
