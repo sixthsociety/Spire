5b5bbcdce8ab438af1a077dd367555f3790bd7bb
Dazdingon <nlareman@gmail.com>
2018-10-23 18:15:53 +0200

    Updated Movement and Input Behaviours
    Input does not comunicate with Movement at the moment

--- BlackSpire/Assets/Behaviours/MovementBehaviour.cs
+++ BlackSpire/Assets/Behaviours/MovementBehaviour.cs
@@ -6,13 +6,63 @@ using UnityEngine;
 
 public class MovementBehaviour : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] float m_DefaultSpeed = 1f;
+        
+    public float speed { get; set; }
+    private Vector3 lookDirection { get; set; }
+    private Vector3 lookPoint { get; set; }
+    private bool isAiming { get; set; }
+
+    public Quaternion lookRotation { get; private set; }
+
+    new private Rigidbody rigidbody;
+    
+    // UNITY UPDATE CALLS
+
+    void OnEnable()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();
+        lookRotation = Quaternion.identity;
+    }
+
+    void FixedUpdate()
+    {
+        if (isAiming)
+        {
+            Look(lookPoint - transform.position);
+            Move(lookDirection);
+        }
+        else
+        {
+            Look(lookDirection);
+            Move(lookDirection);
+        }
+    }
+
+    // PUBLIC ACCESS
+
+    public void SetLookPoint() { throw new System.Exception("unimplemented, set lookPosition instead"); }
+
+    public void SetLookDirection() { throw new System.Exception("unimplemented, set lookDirection instead"); }
+
+    // PRIVATE
+
+    void Move(Vector3 direction)
+    {
+        rigidbody.MovePosition(rigidbody.position + direction * speed);
+        /*
+        //Animation
+        Vector3 localDirection = Quaternion.Inverse(lookRotation) * direction;
+        animator.SetFloat("MoveX", localDirection.x);
+        animator.SetFloat("MoveY", localDirection.z);
+        */
+    }
+
+    void Look(Vector3 direction)
+    {
+        if (direction.sqrMagnitude < Mathf.Epsilon) return;
+        direction.y = 0f;
+        lookRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = lookRotation;
+    }
 }
--- BlackSpire/Assets/Behaviours/UserInputBehaviour.cs
+++ BlackSpire/Assets/Behaviours/UserInputBehaviour.cs
@@ -6,13 +6,55 @@ using UnityEngine;
 
 public class UserInputBehaviour : MonoBehaviour {
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Camera inputCamera { get; set; }
+
+    public Vector3 inputDirection { get; private set; }
+    public Vector3 inputWorldPosition { get; private set; }
+    public bool isAiming { get; private set; }
+
+    // UNITY UPDATE CALLS
+
+    private void OnEnable()
+    {
+        inputCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        UpdateInputDirection();
+
+        isAiming = Input.GetMouseButton(1);
+
+        // responsible for calculating the way the player should be looking
+        if (isAiming)
+        {
+            UpdateWorldPosition();
+        }
+    }
+
+    // PUBLIC ACCESS
+
+    public void DoNothing() { }
+
+    // PRIVATE
+
+    void UpdateInputDirection()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float inputY = Input.GetAxisRaw("Vertical");
+        inputDirection = new Vector3(inputX, 0f, inputY).normalized;
+    }
+
+    void UpdateWorldPosition()
+    {
+        // shoot a ray from cam through the mouse to the ground
+        Ray ray = inputCamera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
+        float rayDistance;
+
+        if (groundPlane.Raycast(ray, out rayDistance))
+        {
+            inputWorldPosition = ray.GetPoint(rayDistance);
+        }
+    }
 }
