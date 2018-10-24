2329df01c4502e63f4321587b7a02ad14fb06bbe
Dazdingon <nlareman@gmail.com>
2018-10-24 08:32:04 +0200

    Updated SpawnBehaviour
    Unused at the moment

--- BlackSpire/Assets/Behaviours/SpawnBehaviour.cs
+++ BlackSpire/Assets/Behaviours/SpawnBehaviour.cs
@@ -5,14 +5,30 @@ using UnityEngine;
 // playerPrefab, enemyPrefabs, towerPrefabs, bullet-pool
 
 public class SpawnBehaviour : MonoBehaviour {
+    [SerializeField]GameObject m_DefaultPrefab;
+    [SerializeField] int m_PoolSize = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    GameObject[] objectPool;
+    int poolIndex = 0;
+
+    // --- UNITY UPDATES ---
+
+    void OnEnable () {
+        if (m_PoolSize > 0)
+            if (objectPool == null || objectPool.Length != m_PoolSize) InitializePool();
+    }
+
+    // --- PUBLIC ---
+
+    // Unused, placeholder
+    public void SpawnAt(Vector3 position, bool pooled = false)
+    {
+        GameObject instance = Instantiate(m_DefaultPrefab);
+        instance.transform.position = position;
+        MovementBehaviour movement = m_DefaultPrefab.GetComponent<MovementBehaviour>();
+    }
+
+    // --- PRIVATE ---
+
+    void InitializePool() { objectPool = new GameObject[m_PoolSize]; }
 }
